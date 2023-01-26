4fb6b226cfc584913a5c7c3bbc0664916944a475
mina1971 <jommina62@gmail.com>
2023-01-26 16:40:00 +0200

    my new land

---
+++ README.md
@@ -0,0 +1,23 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+#Four sections 
+(hard-coded or dynamically).
+#Four navbar items
+ corresponding to each section (dynamically)
+#Clicking the navbar item >>> 
+scrolling  smoothly  viewport to the target section.
+#Clicking the navbar item >>> 
+highlighting the item background for the user to follow easily where s/he is surfing.
+#responsive meneu use @media screen.
---
+++ js/app.js
@@ -0,0 +1,110 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const list = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+function myNewNavbar() {
+ // loop over sections
+  for(sec of sections) {
+    // create list item and enchor tag
+      const listItem = document.createElement('li');
+       // attributes from section id 
+      listItem.innerHTML =`<li><a href="#${sec.id}"data-nav="${sec.id}"class="menu__link">
+      ${sec.dataset.nav}</a></li>`;
+     
+       list.appendChild(listItem);  
+   }
+  }
+    //smooth scrolling ,title
+    //add section.dataset.nav , smooth scrolling ,title
+   list.addEventListener('click',(toSection)=>{
+    toSection.preventDefault();
+    if(toSection.target.dataset.nav){
+      document.getElementById(`${toSection.target.dataset.nav}`).scrollIntoView({behavior:"smooth"})
+    }
+   })
+myNewNavbar();
+
+// Build menu
+
+window.addEventListener("scroll", navLight);
+//scroll to section on link click
+const links = document.querySelectorAll("a.menu__link");
+function navLight() {
+  //loop over section
+  sections.forEach((section) => {
+    //using getBoundingClientRect Return the size of an element and its position relative to the viewport
+    const sectionTop = section.getBoundingClientRect().top;
+    //Selection of items by dataset
+    const sectionTitle = section.getAttribute("data-nav");
+    if (sectionTop > 0 && sectionTop < 250) {
+      // add active class link
+      section.classList.add("your-active-class");
+      //loop over links
+      links.forEach(link => {
+        if (link.textContent === sectionTitle) {
+          link.classList.add("active-class");
+        } else {
+          link.classList.remove("active-class");
+        }
+      })
+    }
+  })  
+}
+navLight();
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build  mini menu using  small list icon
+function miniNavBar() {
+  var nav = document.getElementById("navbar__list");
+  if (nav.className === "nav-top") {
+    nav.className += " show_list";
+  } else {
+    nav.className = "nav-top";
+  }
+}
+// Scroll to section on link click
+
+// Set sections as active
